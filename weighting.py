17d987cbb6b9a3f8c92bbca290288774d704e51b
Henry Bruce <henry.bruce@intel.com>
2021-12-16 15:48:08 -0800

    Refactor weighting

--- weighting.py
+++ weighting.py
@@ -1,16 +1,18 @@
+from dataclasses import dataclass
 import logging
-from typing import NamedTuple
 import pytz
 
-class CourseGroup(NamedTuple):
-    course_id: int
+@dataclass
+class AssignmentWeighting:
+    weighting: int
+    max_score: int
+    score: int
 
 
 class WeightedScoreCalculator:
 
     def __init__(self, courses):
-        self.group_max = {}
-        self.weightings = {}
+        self.assignment_weightings = {}
         self.assignment_groups = {}
         self.equal_weighted_courses = []
         self.logger = logging.getLogger(__name__)
@@ -22,19 +24,18 @@ class WeightedScoreCalculator:
                     w = g.group_weight
                     if w == 0:
                         w = 100
-                        self.equal_weighted_courses.append(course.id)
-                    self.weightings[g.id] = w
+                        if course.id not in self.equal_weighted_courses:
+                            self.equal_weighted_courses.append(course.id)
+                    self.assignment_weightings[g.id] = AssignmentWeighting(w, 0, 0)
                     assignment_group.append(g.id)
                     # print("%s %s %s %d" % (self.course_short_name(c.name), g.name, g.id, w))
                 self.assignment_groups[course.id] = assignment_group
-        for w in self.weightings:
-            self.group_max[w] = 0
-
 
     # Re-calculate weightings in case some some weights are not yet in use
     def update(self, assignments, end_date):
-        for w in self.weightings:
-            self.group_max[w] = 0
+        for gid in self.assignment_weightings:
+            print
+            self.assignment_weightings[gid].max_score = 0
         course_groups = {}
 
         self.logger.info("Weighting first pass")
@@ -44,46 +45,48 @@ class WeightedScoreCalculator:
                 self.logger.info(" {}[{}] = {} ({})".format(assignment.get_course_name(), group_id, assignment.get_name(), id))
                 self.logger.info(" - Checking assignment")
                 self.logger.info("   - valid assignment: {}".format(assignment.is_valid))
-                self.logger.info("   - valid group: {}".format(group_id in self.group_max))
+                self.logger.info("   - valid group: {}".format(group_id in self.assignment_groups[assignment.course_id]))
                 self.logger.info("   - graded: {}".format(assignment.is_graded()))
                 self.logger.info("   - score: {}".format(assignment.get_score()))
-                if assignment.is_graded() and (group_id in self.group_max):
+                if assignment.is_graded() and (group_id in self.assignment_weightings):
                     course_id = assignment.course_id
                     if not course_id in course_groups:
                         course_groups[course_id] = []
                     course_group = course_groups[course_id]
                     if group_id not in course_group:
                         course_group.append(group_id)
-                    self.group_max[group_id] = self.group_max[group_id] + assignment.get_points_possible()
+                    self.assignment_weightings[group_id].max_score += assignment.get_points_possible()
+                    self.assignment_weightings[group_id].score += assignment.get_raw_score()
 
         self.logger.info("Weighting second pass")
         for course_id in course_groups:
-            group = course_groups[course_id]
-            # print("{} {}".format(course_id, group))
-            if len(group) == 1:
-                for w in self.assignment_groups.get(course_id):
-                    self.weightings[w] = 100
+            groups = course_groups[course_id]
+            # print("{} {}".format(course_id, groups))
+            if len(groups) == 1:
+                for gid in self.assignment_groups.get(course_id):
+                    self.assignment_weightings[gid].weighting = 100
                 if course_id not in self.equal_weighted_courses:
                     self.equal_weighted_courses.append(course_id)
             if course_id in self.equal_weighted_courses:
                 points = 0
-                for i in self.assignment_groups.get(course_id):
-                    points = points + self.group_max[i]
-                for i in self.assignment_groups.get(course_id):
-                    self.group_max[i] = points
+                for gid in self.assignment_groups.get(course_id):
+                    points = points + self.assignment_weightings[gid].max_score
+                for gid in self.assignment_groups.get(course_id):
+                    self.assignment_weightings[gid].max_score = points
 
     def includes_assignment(self, assignment):
         group_id = assignment.get_group()
-        return group_id in self.group_max
+        return group_id in self.assignment_weightings
 
     def missing_gain(self, assignment):
-        self.logger.info("Gain: {} [{}] {} {} {}".format(assignment.course_name, assignment.get_name(), assignment.get_points_dropped(), self.weightings[assignment.group], self.group_max[assignment.group]))
+        self.logger.info("Gain: {} [{}] {} {} {}".format(assignment.course_name, assignment.get_name(), assignment.get_points_dropped(), self.assignment_weightings[assignment.group].weighting, self.assignment_weightings[assignment.group].max_score))
         possible_gain = 0
-        if self.group_max[assignment.group] + assignment.get_points_possible() > 0:
-            possible_gain = int((self.weightings[assignment.group] * assignment.get_points_dropped()) / (self.group_max[assignment.group] + assignment.get_points_possible()))
+        gid = assignment.group
+        if self.assignment_weightings[gid].max_score + assignment.get_points_possible() > 0:
+            possible_gain = int((self.assignment_weightings[gid].weighting * assignment.get_points_dropped()) / (self.assignment_weightings[gid].max_score + assignment.get_points_possible()))
         return possible_gain
 
     def marked_gain(self, assignment):
-        possible_gain = int((self.weightings[assignment.group] * assignment.get_points_dropped()) / self.group_max[assignment.group])
+        possible_gain = int((self.assignment_weightings[assignment.group].weighting * assignment.get_points_dropped()) / self.assignment_weightings[assignment.group].max_score)
         return possible_gain
 
